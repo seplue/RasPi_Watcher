9b6ad192bdde30df4d61d9d19c48c6ccc80e2738
seplue <severin.pluess@bluewin.ch>
2022-02-18 12:14:49 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,72 @@
+from bs4 import BeautifulSoup
+import lxml
+import requests
+import re
+import time
+
+print('started')
+print('Last checked:')
+
+while True:
+    # pi-shop.ch
+    url_pishop_zero2 = 'https://www.pi-shop.ch/raspberry-pi-zero-2-w?src=raspberrypi'
+    url_pishop_control = 'https://www.pi-shop.ch/raspberry-pi-zero-2-w-starter-kit'
+    url = url_pishop_zero2
+
+    source = requests.get(url).text
+    soup = BeautifulSoup(source, 'lxml')
+
+    av_text = soup.find('p', class_='availability').text
+
+    # print(av_text)
+    if av_text != 'Verfügbarkeit: Zur Zeit nicht an Lager':
+        print('Its available at https://www.pi-shop.ch/raspberry-pi-zero-2-w?src=raspberrypi!')
+    if av_text == 'Verfügbarkeit: Zur Zeit nicht an Lager':
+        # print('Its not available at pi-shop.ch')
+        pass
+
+    # berrybase.ch
+    url_berrybase_zero2 = 'https://www.berrybase.ch/detail/index/sArticle/9202?src=raspberrypi'
+    url_berrybase_control = ''
+    url = url_berrybase_zero2
+
+    source = requests.get(url).text
+    soup = BeautifulSoup(source, 'lxml')
+
+    product = soup.find('div', class_='content product--details')
+    av_text = product.find('div', class_='alert--content').text
+
+    # print(av_text)
+    if av_text != ' Dieser Artikel steht derzeit nicht zur Verfügung! ':
+        print('Its available at https://www.berrybase.ch/detail/index/sArticle/9202?src=raspberrypi!')
+    if av_text == ' Dieser Artikel steht derzeit nicht zur Verfügung! ':
+        # print('Its not available at berrybase.ch')
+        pass
+
+    # welectron.com
+    url_welectron_na = 'https://www.welectron.com/Raspberry-Pi-Zero-W-mit-loser-Stiftleiste'  # status 0 = not available
+    url_welectron_zero2 = 'https://www.welectron.com/Raspberry-Pi-Zero-2-W'  # status 1 = on the way
+    url_welectron_a = 'https://www.welectron.com/Raspberry-Pi-Pico'  # status 2 = available
+    url = url_welectron_zero2
+
+    source = requests.get(url).text
+    soup = BeautifulSoup(source, 'lxml')
+
+    print('bei welectron:', end='\r')
+    status_0 = soup.find('span', class_='status status-0')
+    if status_0:
+        # print(status_0.text)
+        pass
+    status_1 = soup.find('span', class_='status status-1')
+    if status_1:
+        # print(status_1.text)
+        pass
+    status_2 = soup.find('span', class_='status status-2')
+    if status_2:
+        print(status_2.text)
+
+    t = time.localtime()
+    current_time = time.strftime("%H:%M:%S", t)
+    print('\r' + current_time, end='')
+
+    time.sleep(60)
